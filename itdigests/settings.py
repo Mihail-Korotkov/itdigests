8195d7a7e15d8a79e5b5177000b8f1cf32f3b970
Mihail-Korotkov <mixail.korotkov.01@bk.ru>
2026-08-31 18:28:21 +0300

    Models ready,  database changed

--- itdigests/settings.py
+++ itdigests/settings.py
@@ -78,11 +78,15 @@ WSGI_APPLICATION = 'itdigests.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dbfordijest',
+        'USER': 'misha',
+        'PASSWORD': 'mishana',
+        'HOST': 'localhost',
+        'PORT': '5433',
+
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
--- main/models.py
+++ main/models.py
@@ -1,3 +1,35 @@
+from tabnanny import verbose
+
 from django.db import models
 
 # Create your models here.
+class DaylyDijest(models.Model):
+    title = models.CharField(max_length=200, default="IT-Дайджест")
+    summary = models.TextField()
+    created_at =models.DateTimeField(auto_now_add=True)
+    is_published = models.BooleanField(default=True)
+
+    class meta:
+        ordering  = ['-created_at']
+        verbose_name = 'IT-Дайджест'
+        verbose_name_plural = 'IT-Дайджесты'
+
+    def __str__(self):
+        return f"Дайджест от {self.created_at.strftime('%d.%m.%Y')}"
+
+class Article(models.Model):
+    digist = models.ForeignKey(DaylyDijest, on_delete=models.CASCADE,related_name='articles')
+    title = models.CharField(max_length=500)
+    url = models.URLField(unique=True)
+    source = models.CharField(max_length=100)
+    published_at = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    
+    class meta:
+        ordering  = ['-Published_at']
+        verbose_name = 'Статья'
+        verbose_name_plural = 'Статьи'
+
+    def __str__(self):
+        return self.title
